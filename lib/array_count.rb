572e1c0293c7eb46457c6b89c69e88f396f3d7d3
maxwellbenton <maxwellbenton@gmail.com>
2020-03-18 14:08:55 -0400

    set starter code

---
+++ lib/array_count.rb
@@ -0,0 +1,7 @@
+def count_strings(array)
+  # Return the total number of strings in the provided array using the count enumerable
+end
+
+def count_empty_strings(array)
+  # Return the total number of EMPTY strings in the provided array using the count enumerable
+end
